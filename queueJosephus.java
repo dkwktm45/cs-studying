fd67345d95aa46153ceb7ec47146e85ad8fb8e2c
dkwktm45 <dkwktm45@naver.com>
2022-12-23 19:03:34 +0900

    Josephus

---
+++ queueJosephus.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class queueJosephus {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+    Scanner sc = new Scanner(System.in);
+
+    int ArrayCnt = sc.nextInt();
+    int loopNum = sc.nextInt();
+    List<Integer> result = new ArrayList<>();
+    ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+    for (int i = 1; i <= ArrayCnt; i++) queue.add(i);
+    
+    int num =0;
+    while (!queue.isEmpty()) {
+      num++;
+      int tmp = queue.poll();
+      if(num%loopNum==0){
+        result.add(tmp);
+      }else{
+        queue.add(tmp);
+      }
+    }
+
+    System.out.println(result.toString().replace("[", "<").replace("]", ">"));
+  }
+}
+}
