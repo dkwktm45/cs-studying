d5874f6bf788ca36cf70e831ca7bf47b4a591c8d
dkwktm45 <dkwktm45@naver.com>
2023-01-05 23:03:54 +0900

    Stack 10799

--- Stack/Stack10799.java
+++ Stack/Stack10799.java
@@ -1,5 +1,3 @@
-package Stack;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,27 +6,23 @@ import java.util.Stack;
 public class Stack10799 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] solution = br.readLine().toCharArray();
+        String solution = br.readLine();
         Stack<Character> stack = new Stack();
-        int cnt = 0;
-        int mc = 0;
-        int la = 0;
-        for(char s : solution){
-            if(stack.isEmpty()){
-                stack.add(s);
-            }else if(stack.peek().equals('(')){
-                if(s == ')'){
-                    la++;
-                    stack.add(s);
-                    if (stack.size() > 2) cnt += la*mc;
-                }else{
-                    mc++;
+        int result = 0;
+        for (int i = 0; i < solution.length(); i++) {
+            if (solution.charAt(i) == '(') { // 열린 괄호면 스택에 추가.
+                stack.push('(');
+                continue;
+            }
+            if (solution.charAt(i) == ')') { // 닫힌 괄호일 경우,
+                stack.pop(); // 일단 stack에서 pop을 실행.
+ 
+                if (solution.charAt(i - 1) == '(') { // 그 전 괄호가 열린 괄호면 레이저를 의미.
+                    result += stack.size(); // 현재 stack의 사이즈만큼 더해 줌.
+                } else { // 그 전 괄호가 닫힌 괄호면 레이저가 아님.
+                    result++; // 단순히 1을 더해 줌.
                 }
-            }else{
-                stack.add(s);
-                mc--;
             }
         }
-        System.out.println(cnt);
     }
 }
