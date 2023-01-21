4a4415df48a1ef9da6bf64e6404aea6716eec557
dkwktm45 <dkwktm45@naver.com>
2023-01-21 19:01:14 +0900

    보류

---
+++ DFS/BFS/B13549.java
@@ -0,0 +1,26 @@
+package DFS.BFS;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.event.AncestorEvent;
+
+public class B13549{
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int result = 0;
+        while(x != y){
+            
+            if(x*2 < y){
+                x *=2;
+            }else if(){
+
+            }
+            result++;
+        }        
+
+    }
+}
